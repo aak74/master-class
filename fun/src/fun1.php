d669a59c91eb192863d31e7c1db1d3b24b19770f
Andrew Kopylov <aakopylov@mail.ru>
2017-09-21 23:15:40 +0300

    first commit

---
+++ fun/src/fun1.php
@@ -0,0 +1,52 @@
+<?php
+
+/* Заменяет элементы входного массива, содержащие подстроки
+   «серебряный обрез», «золотой обрез», «с серебряным обрезом»,
+   «с золотым обрезом», «серебро» или «золото», на название материала,
+   присутствующего в названии, т.е. на «серебро» или «золото».  */
+function transform($a)
+{
+    print_r($a);
+    $str_obrez = [];
+    $kol_obrez=0;
+    for ($i=0; $i<count($a); $i++) {
+        echo $i;
+        $n1 = strpos($a[$i], "серебро");
+        $n2 = strpos($a[$i], "золото");
+        $n3 = strpos($a[$i], "серебряный обрез");
+        $n4 = strpos($a[$i], "золотой обрез");
+        $n5 = strpos($a[$i], "с серебряным обрезом");
+        $n6 = strpos($a[$i], "с золотым обрезом");
+        echo $n3;
+        if ($n1) {
+            $str_obrez[$kol_obrez] = substr($a[$i], $n1, strlen("серебро"));
+        }
+        if ($n2) {
+            $str_obrez[$kol_obrez] = substr($a[$i], $n2, strlen("золото"));
+        }
+        if ($n3) {
+            $str_obrez[$kol_obrez] = substr($a[$i], $n3, strlen("серебряный обрез"));
+        }
+        if ($n4) {
+            $str_obrez[$kol_obrez] = substr($a[$i], $n4, strlen("золотой обрез"));
+        }
+        if ($n5) {
+            $str_obrez[$kol_obrez] = substr($a[$i], $n5, strlen("с серебряным обрезом"));
+        }
+        if ($n6) {
+            $str_obrez[$kol_obrez] = substr($a[$i], $n6, strlen("с золотым обрезом"));
+        }
+        $kol_obrez++;
+    }
+
+    for ($j=0; $j<count($str_obrez); $j++) {
+        if ($str_obrez[$j]=="с серебряным обрезом") {
+            $str_obrez[$j] = "серебро";
+        }
+        if ($str_obrez[$j]=="с золотым обрезом") {
+            $str_obrez[$j] = "золото";
+        }
+    }
+
+    return $str_obrez;
+}
---
+++ fun/tests/FunTest.php
@@ -0,0 +1,29 @@
+<?php
+//  require_once('../../src/fun1.php');
+include_once __DIR__ . '/../src/fun4.php';
+
+class FunTest extends \PHPUnit\Framework\TestCase
+{
+
+    public function testSilver()
+    {
+        $pattern = ['серебро'];
+        $this->assertEquals($pattern, transform([' серебро']));
+        $this->assertEquals($pattern, transform(['серебро']));
+        $this->assertEquals($pattern, transform([' серебряный обрез']));
+        $this->assertEquals($pattern, transform(['серебряный обрез']));
+        $this->assertEquals($pattern, transform(['с серебряным обрезом']));
+        $this->assertEquals($pattern, transform([' с серебряным обрезом']));
+    }
+
+    public function testGold()
+    {
+        $pattern = ['золото'];
+        $this->assertEquals($pattern, transform([' золото']));
+        $this->assertEquals($pattern, transform(['золото']));
+        $this->assertEquals($pattern, transform(['золотой обрез']));
+        $this->assertEquals($pattern, transform([' золотой обрез']));
+        $this->assertEquals($pattern, transform(['с золотым обрезом']));
+        $this->assertEquals($pattern, transform([' с золотым обрезом']));
+    }
+}
